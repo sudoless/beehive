f6e3c6878db22f47c0addb580f6fdfb33b6f9be6
Alexandru-Paul Copil <alex+git@sudoless.org>
2025-08-14 09:21:53 +0300

    Implement Grouper.With

--- pkg/beehive/group.go
+++ pkg/beehive/group.go
@@ -4,9 +4,19 @@ import "go.sdls.io/beehive/internal/trie"
 
 // Grouper implements the abstraction layer for applying a handler or middleware on a group of routes.
 type Grouper interface {
+	// Group adds the given middleware to a *new* Grouper and returns it. The current Grouper is not modified.
 	Group(pathPrefix string, middleware ...HandlerFunc) Grouper
+
+	// Handle takes all the added middleware and the given handlers and registers them.
 	Handle(method, path string, handlers ...HandlerFunc) Grouper
+
+	// HandleAny takes all the added middleware and the given handlers and registers them on all given methods.
 	HandleAny(methods []string, path string, handlers ...HandlerFunc) Grouper
+
+	// With appends the given middlewar to the current Grouper and returns itself (for chaining). The changes here will
+	// be "reflected" on Grouper relaying on parent chaining obtained by calling Group, With, Handle, etc. before or
+	// after the call to With.
+	With(middleware ...HandlerFunc) Grouper
 }
 
 type group struct {
@@ -30,6 +40,11 @@ func (g *group) Handle(method, path string, handlers ...HandlerFunc) Grouper {
 
 func (g *group) HandleAny(methods []string, path string, handlers ...HandlerFunc) Grouper {
 	g.parent.HandleAny(methods, g.prefix+path, append(g.middleware, handlers...)...)
+	return g
+}
+
+func (g *group) With(middleware ...HandlerFunc) Grouper {
+	g.middleware = append(g.middleware, middleware...)
 	return g
 }
 
@@ -95,4 +110,8 @@ func (router *Router) HandleAny(methods []string, path string, handlers ...Handl
 	}
 
 	return router
+}
+
+func (router *Router) With(middleware ...HandlerFunc) Grouper {
+	panic("beehive: router does not store middleware, use a Group/Grouper first")
 }
